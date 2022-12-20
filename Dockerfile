b8851289343af10245e66072c23c4a09cdee6607
gsoulat <gsoulat31@gmail.com>
2022-12-20 21:43:59 +0100

    run

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
 FROM python:3.10.6
-
-RUN apt update && apt install -y cron vim
-
 WORKDIR /
 ENV VIRTUAL_ENV=/opt/venv
 RUN python3 -m venv $VIRTUAL_ENV
@@ -13,10 +10,6 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 
-
 # Run the application:
 COPY . .
-
-ADD main.py /
-RUN chmod a+x main.py
-ENTRYPOINT  ["cron", "-f"]
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,8 +1,59 @@
+import discord
+from discord.ext import commands, tasks
+import pandas as pd
+TOKEN = 'MTAxOTI0MzgxMzkyMDg0NTgyNg.GLsfnV.q1SyVwcw196qpszKLMst_MxraBUX5HuA2eD-b8'
+CHANNEL_ID = 1050490216617619459
 
-import logging
-import datetime
+intents = discord.Intents.all()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@tasks.loop(minutes=1)
+async def test():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send("test")
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    test.start()
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        
+    if message.content.startswith('!test'):
+        await message.channel.send(f'Hello! welcome to best team, MVP')
+        
+    if message.content.startswith('!tod'):
+        list_members = []
+        list_members_with_power = []
+        f1 = []
+        for guild in client.guilds:
+            async for member in guild.fetch_members(limit=150):
+                unlisted = ["MEE6", "MVP-BOT","Rythm"]
+                try:
+                    s = member.nick
+                    s.find('F1')
+                    if member.name not in unlisted and 'F1' in member.nick:
+                        list_members.append(member.name)
+                        list_members_with_power.append(member.nick)
+                        f1.append(s[s.find('F1')+3:])
+                        df_members = pd.Series(list_members)
+                        df_members_with_power = pd.Series(list_members_with_power)
+                        frame = { 'members': df_members, 'nickname': df_members_with_power, 'f1': f1}
+                        df = pd.DataFrame(frame)
+                        df = df.sort_values(by=['f1'], ascending=False)
+                except Exception:
+                    print('pas de F1 trouvé')
+        for i in range(0,len(df),2):
+                await message.channel.send(f'TEAM : {i}')
+                await message.channel.send(f'{df.iloc[i:i+2]}')
 
 
-
-logging.INFO(f"dehors Running script at {datetime.now()}")
-
+client.run(TOKEN)
